33d07d230a57d7f47a2f8779559243001b7b0523
EDZA09 <eduarstiven2001@gmail.com>
2023-12-22 10:39:15 -0500

    Build(VideoController): Elimino anotaciones de Routing local para utilizar el de routes.yaml

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -4,13 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
 
 class VideoController extends AbstractController
 {
-    /**
-     * @Route("/video", name="app_video")
-     */
     public function index(): JsonResponse
     {
         return $this->json([
