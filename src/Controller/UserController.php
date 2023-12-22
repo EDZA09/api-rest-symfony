40a89ebde8ebe19957dc1e28587f214917ff4f39
EDZA09 <eduarstiven2001@gmail.com>
2023-12-22 10:35:24 -0500

    Build(UserController): Elimino anotaciones de Routing local para utilizar el de routes.yaml

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,13 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/user", name="app_user")
-     */
+
     public function index(): JsonResponse
     {
         return $this->json([
