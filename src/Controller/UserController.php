d6c09ea173e891d788cfa75ac30d52c11c9f037e
EDZA09 <eduarstiven2001@gmail.com>
2023-12-28 18:17:01 -0500

    Feat(UserController): Importo las entidades para acceder a su repositorio y así mostrarlas en pantalla.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,12 +4,25 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\User;
+use App\Entity\Video;
 
 class UserController extends AbstractController
 {
-
+    
     public function index(): JsonResponse
     {
+        
+        $user_repo = $this->getDoctrine()->getRepository(User::class);
+        $video_repo = $this->getDoctrine()->getRepository(User::class);
+        
+        $users = $user_repo->findAll();
+        
+        foreach ($users as $user){
+            echo "<h1>{$user->getName()} {$user->getSurname()}</h1>";
+        }
+        
+        die();
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/UserController.php',
