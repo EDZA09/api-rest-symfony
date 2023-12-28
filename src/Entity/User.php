e66f80bb11955a79d1abab300b89ecfc955fa33c
EDZA09 <eduarstiven2001@gmail.com>
2023-12-28 17:53:21 -0500

    Build(Entity): Creo la relacion de uno a muchos entre las entidades

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 
 /**
  * Users
@@ -62,6 +64,17 @@ class User
      * @ORM\Column(name="created_at", type="datetime", nullable=true, options={"default"="CURRENT_TIMESTAMP"})
      */
     private $createdAt = 'CURRENT_TIMESTAMP';
+    
+    /**
+     * 
+     * @ORM\OneToMany(targetEntity="App\Entity\Video", mappedBy="user")
+     */
+    private $videos;
+    
+    public function __construct(){
+        $this->videos = new ArrayCollection();
+    }
+    
 
     public function getId(): ?int
     {
