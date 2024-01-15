b182230fac1bf21ad81c51d9f2514644dcdae304
EDZA09 <eduarstiven2001@gmail.com>
2024-01-15 10:59:10 -0500

    Feat(JwtAuth): Personalizo el retorno en base al valor de gettoken y signup

--- src/services/JwtAuth.php
+++ src/services/JwtAuth.php
@@ -15,7 +15,7 @@ class JwtAuth {
     $this->key = "esta_es_la_secret.12496285";
   }
 
-  public function signup($email, $password, $gettoken){
+  public function signup($email, $password, $gettoken = null){
     // Comprobar si el usuario existe
     $user = $this->manager->geRepositoty(User::class)->findOneBy([
       'email' => $email,
@@ -26,7 +26,6 @@ class JwtAuth {
     if(is_object($user)){
       $signup = true;
     }
-    // Comprobar el flag gettoken, condición
     if($signup){
       $token = [
         'sub' => $user->id,
@@ -36,11 +35,24 @@ class JwtAuth {
         'iat' => time(),
         'exp'=> time() + (7*24*60*60)
       ];
-
+      
+      // Comprobar el flag gettoken, condición
       $jwt = JWT::encode($token, $this->key, 'HS256');
+      if(!empty(gettoken)){
+        $data = $jwt;
+      } else {
+        $decoded = JWT::decode($jwt, $this->key, 'HS256');
+        $data = $decoded;
+      }
+    } else {
+      $data = [
+        'status' => 'error',
+        'code' => 400,
+        'message' => 'Login Incorrecto'
+      ];
     }
 
     // Devolver datos
-    return "Hola mundo, desde el servicio";
+    return $data;
   }
 }
