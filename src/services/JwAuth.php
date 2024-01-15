d6229e1d40c523b4eabd5d4b1b5bc0706805faf3
EDZA09 <eduarstiven2001@gmail.com>
2024-01-14 19:03:47 -0500

    Build(Services): Creo la carpeta para los servicios como el JwtAuth

---
+++ src/services/JwAuth.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+use Firebase\JWT\JWT;
+use App\Entity\User;
+
+class JwtAuth {
+  public function signup(){
+    return "Hola mundo, desde el servicio";
+  }
+}
+
