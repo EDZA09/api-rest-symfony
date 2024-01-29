f87ff30e4507e8f8fe397aacbd71ea23b5e77e0b
EDZA09 <eduarstiven2001@gmail.com>
2024-01-29 12:02:41 -0500

    Build(VideoController): Importo los paquetes necesarios para desarrollar la lógica

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -3,7 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Constraints\Email;
+use App\Entity\User;
+use App\Entity\Video;
+use App\Services\JwtAuth;
+
 
 class VideoController extends AbstractController
 {
